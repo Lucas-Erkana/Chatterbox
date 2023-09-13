15794ea2810b52bafe240fda8874a2c4efbb0c10
Lucas David Erkana <lderkana21x@gmail.com>
2023-09-13 18:59:20 +0200

    View Signin

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Chatterbox</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
