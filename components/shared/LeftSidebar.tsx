15e7181aa4d75ec3537359a7f4f79847aa7d8ede
Lucas David Erkana <lderkana21x@gmail.com>
2023-09-16 11:43:07 +0200

    left side bar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { sidebarLinks } from "@/constants";
 
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>
+  return (
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+      {sidebarLinks.map((link) => (
+        <Link
+        href={link.route}
+        key={link.label}
+        className={"leftsidebar_link"}
+      >
+        <Image
+          src={link.imgURL}
+          alt={link.label}
+          width={24}
+          height={24}
+        />
+
+        <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+      </Link>
+      ))}
+      </div>
+    </section>
+  )
 }
 
 export default LeftSidebar;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,7 +9,29 @@ function Topbar() {
         <Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Chatterbox</p>
       </Link>
-
+      <div className='flex items-center gap-1'>
+      <div className='block md:hidden'>
+      <SignedIn>
+        <SignOutButton>
+          <div className='flex cursor-pointer'>
+            <Image
+              src='/assets/logout.svg'
+              alt='logout'
+              width={24}
+              height={24}
+            />
+          </div>
+        </SignOutButton>
+      </SignedIn>
+      </div>
+      <OrganizationSwitcher
+          appearance={{
+              elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
     </nav>
   );
   }
