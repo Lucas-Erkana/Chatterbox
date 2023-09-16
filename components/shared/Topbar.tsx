78ea9c05f6146ffa9b54b873d268b0c35a1b3330
Lucas David Erkana <lderkana21x@gmail.com>
2023-09-16 12:12:13 +0200

    Update logo display

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,7 +7,7 @@ function Topbar() {
   return (
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.svg' alt='logo' width={28} height={28} />
+        <Image src='./logo.svg' alt='logo' width={28} height={28} />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
       </Link>
 
