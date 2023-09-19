c02f804ed323c9b895b1cd86492170718b17f995
Lucas David Erkana <lderkana21x@gmail.com>
2023-09-19 21:57:33 +0200

    Bottom bar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -18,8 +18,8 @@ const LeftSidebar = () => {
       <div className='flex w-full flex-1 flex-col gap-6 px-6'>
         {sidebarLinks.map((link) => {
           const isActive =
-            (pathname.includes(link.route) && link.route.length > 1) ||
-            pathname === link.route;
+            (pathname.includes(link.route) && link.route.length >
+             1) || pathname === link.route;
 
           if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
