f493f026293751f88208212ddb79061cc95c0f0d
Lucas David Erkana <lderkana21x@gmail.com>
2023-09-20 00:27:09 +0200

    Update AccountProfile.tsx

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { useForm } from "react-hook-form";
@@ -6,17 +6,15 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod"
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 
 interface Props {
@@ -30,7 +28,8 @@ interface Props {
     };
     btnTitle: string;
 }
-const AccountProfile = ({user, btnTitle}) => {
+const AccountProfile = ({user, btnTitle}: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -41,11 +40,26 @@ const AccountProfile = ({user, btnTitle}) => {
         }
     })
 
-    const handleImage = (e: ChangeEvent,
-      fieldChange: (value: string) => void
-    ) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>,
+      fieldChange: (value: string) => void) => {
       e.preventDefault();
-    }
+  
+      const fileReader = new FileReader();
+  
+      if (e.target.files && e.target.files.length > 0) {
+        const file = e.target.files[0];
+        setFiles(Array.from(e.target.files));
+  
+        if (!file.type.includes("image")) return;
+  
+        fileReader.onload = async (event) => {
+          const imageDataUrl = event.target?.result?.toString() || '';
+          fieldChange(imageDataUrl);
+        };
+  
+        fileReader.readAsDataURL(file);
+      }
+    };
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
         // Do something with the form values.
