7f9c58b72818f07f987476f7b97fdd19684ea3c1
Lucas David Erkana <lderkana21x@gmail.com>
2023-10-09 19:10:58 +0200

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,37 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
-
-export async function updateUser():Promise<void> {
-  connectToDB();
-
   
-}
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string,
+  ): Promise<void> {
+    try {
+      connectToDB();
+  
+      await User.findOneAndUpdate(
+        { id: userId },
+        {
+          username: username.toLowerCase(),
+          name,
+          bio,
+          image,
+          onboarded: true,
+        },
+        { upsert: true }
+      );
+  
+      if (path === "/profile/edit") {
+        revalidatePath(path);
+      }
+    } catch (error: any) {
+      throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+  }
+  
